36d23393e2946e8f87fc8d9ed3ffb284174a3758
lemin194 <leminhduc190204@gmail.com>
2024-08-03 20:29:06 +0700

    add apis :v

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,15 @@ dependencies {
     implementation("androidx.camera:camera-view:$cameraxVersion")
     implementation("androidx.camera:camera-extensions:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
+
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+
+    // define any required OkHttp artifacts without version
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
+    implementation("com.google.android.gms:play-services-cronet:18.0.1")
 }

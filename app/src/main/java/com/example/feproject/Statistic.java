e5e6eed6940f812a87ecf9777fb36b78592421d5
BlackRose484 <hungnbc2@gmail.com>
2024-08-04 13:28:48 +0700

    [style] android: connect with camera

--- app/src/main/java/com/example/feproject/Statistic.java
+++ app/src/main/java/com/example/feproject/Statistic.java
@@ -2,6 +2,8 @@ package com.example.feproject;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
@@ -21,16 +23,17 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class Statistic extends AppCompatActivity {
 
-    Button bar_chart_btn, pie_chart_btn;
     @SuppressLint("MissingInflatedId")
-    private List<String> xValues = Arrays.asList("A", "B", "C", "D", "E");
-
+    List<String> xValues = Arrays.asList("Correct", "Forward", "Backward", "Bending");
+    SQLiteDatabase database;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +46,39 @@ public class Statistic extends AppCompatActivity {
             return insets;
         });
 
+        database = openOrCreateDatabase("statistics", MODE_PRIVATE, null);
+        try {
+            database.execSQL("CREATE TABLE IF NOT EXISTS statistics (day VARCHAR ,correct REAL, forward REAL, backward REAL, bending REAL)");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Cursor c = database.query("statistics", null, null, null, null, null, null);
+        c.moveToNext();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDate = sdf.format(new Date());
+        float correct = 0;
+        float forward = 0;
+        float backward = 0;
+        float bending = 0;
+        while (c.moveToNext()) {
+            String day = c.getString(0);
+            if (day.equals(currentDate)) {
+                correct += c.getFloat(1);
+                forward += c.getFloat(2);
+                backward += c.getFloat(3);
+                bending += c.getFloat(4);
+            }
+        }
         BarChart barChart = findViewById(R.id.bar_chart);
         barChart.setTouchEnabled(true);
         barChart.getAxisRight().setDrawLabels(false);
 
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0, 10f));
-        entries.add(new BarEntry(1, 20f));
-        entries.add(new BarEntry(2, 30f));
-        entries.add(new BarEntry(3, 40f));
-        entries.add(new BarEntry(4, 50f));
+        entries.add(new BarEntry(0, correct));
+        entries.add(new BarEntry(1, forward));
+        entries.add(new BarEntry(2, backward));
+        entries.add(new BarEntry(3, bending));
 
         YAxis yAxis = barChart.getAxisLeft();
         yAxis.setAxisMinimum(0);
